00443347d2128970873dd3ead9dfc6817e4026ec
Zypr <44808225+ZyprA@users.noreply.github.com>
2025-08-16 12:47:33 +0900

    fix: change event registration method visibility to public in EventStreamer

--- src/main/java/net/zypr/modernBattleGame/api/executor/BattleGameExecutor.java
+++ src/main/java/net/zypr/modernBattleGame/api/executor/BattleGameExecutor.java
@@ -1,18 +1,23 @@
 package net.zypr.modernBattleGame.api.executor;
 
 import net.zypr.modernBattleGame.api.game.BattleGame;
-import net.zypr.modernBattleGame.api.player.GamePlayer;
 import net.zypr.modernBattleGame.internal.BattleGameScheduler;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class ModernBattleGameExecutor {
+public class BattleGameExecutor {
     private final BattleGameScheduler battleGameScheduler;
+    private final BattleGame<?> battleGame;
 
-    public ModernBattleGameExecutor(BattleGame<?> battleGame, JavaPlugin plugin) {
+    public BattleGameExecutor(BattleGame<?> battleGame, JavaPlugin plugin) {
+        this.battleGame = battleGame;
         this.battleGameScheduler = new BattleGameScheduler(battleGame, plugin);
     }
 
     public void start() {
         this.battleGameScheduler.start();
+    }
+
+    public BattleGame<?> getGameInstance() {
+        return this.battleGame;
     }
 }
